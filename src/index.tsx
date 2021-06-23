965bd55225e1c9d5614ee3a0ae985681c4c928db
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2021-06-22 19:13:34 -0500

    📦 ADD: styles generales sass

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { App } from '~/App';
 import reportWebVitals from '~/reportWebVitals';
 
+import '~/assets/styles/normalize.css';
+import '~/assets/styles/global.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
