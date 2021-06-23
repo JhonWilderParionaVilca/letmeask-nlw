2fb7bb32797dbe8d893cbe1fb3ac9c4f51f72f4d
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2021-06-23 03:05:02 -0500

    📚UPDATE: add link redirect

--- src/pages/NewSala/NewSala.tsx
+++ src/pages/NewSala/NewSala.tsx
@@ -1,11 +1,16 @@
 import { FormSigIn } from '~/components';
 import { Aside } from '~/container';
 
+import style from './NewSala.module.scss';
+
 export function NewSala() {
   return (
     <Aside>
-      <h2>Cree una nueva sala</h2>
+      <h2 className={style.title}>Cree una nueva sala</h2>
       <FormSigIn placeholder="Nombre de la sala" textBtn="Crear sala" />
+      <p className={style.returnParragraph}>
+        ¿Quiere entrar en una sala existente? <a href="/">Click aqui</a>
+      </p>
     </Aside>
   );
 }
