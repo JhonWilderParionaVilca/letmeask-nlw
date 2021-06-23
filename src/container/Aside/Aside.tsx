47e3368a40a6b9f8c55522a43ee83d2a7a1565d1
wilderPariona <jhon.pariona.27@unsch.edu.pe>
2021-06-22 22:55:57 -0500

    🧔ADD container Aside

---
+++ src/container/Aside/Aside.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { MessageImg } from '~/assets/img';
+
+import style from './aside.module.scss';
+
+type AsideProps = {
+  children: React.ReactNode;
+};
+
+export function Aside({ children }: AsideProps) {
+  return (
+    <div className={style.container}>
+      <aside className={style.aside}>
+        <MessageImg />
+
+        <div>
+          <strong className={style.asideTitle}>
+            <span role="img" aria-label="emoji pensativo">
+              🤔
+            </span>
+            Toda Pregunta tiene
+            <span className={style.asideTitle}>una respuesta </span>
+          </strong>
+          <p className={style.asideParrafo}>
+            Aprenda y comparta conocimiento con otras personas
+          </p>
+        </div>
+      </aside>
+      <main className={style.main}>{children}</main>
+    </div>
+  );
+}
